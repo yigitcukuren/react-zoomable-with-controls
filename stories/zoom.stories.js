395835e3dd9ad0f0fe40cda6c32ce1b28686b106
Yigit Cukuren <yigitcukuren@jotform.com>
2019-11-18 22:01:39 +0300

    fix: test story fix to trigger semantic release

--- stories/zoom.stories.js
+++ stories/zoom.stories.js
@@ -3,7 +3,7 @@ import { Zoom } from '../src/index';
 
 export default { title: 'Zoom' };
 
-export const withAStandardDiv = () => <Zoom><div>Hello Button</div></Zoom>;
+export const withAStandardDiv = () => <Zoom><div>Hello Button2</div></Zoom>;
 
 export const withAnImage = () => {
   const Image = () => <img src="https://m.media-amazon.com/images/I/91FxiyDiVtL._SS500_.jpg" />;
