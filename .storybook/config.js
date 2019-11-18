92132546f1f87022e01d3ad4baa6ccb2487dade6
Yigit Cukuren <yigitcukuren@jotform.com>
2019-11-18 20:23:16 +0300

    Storybook added

---
+++ .storybook/config.js
@@ -0,0 +1,2 @@
+import { configure } from '@storybook/react';
+configure(require.context('../stories', true, /\.stories\.js$/), module);
---
+++ stories/zoom.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Zoom } from '../src/index';
+
+export default { title: 'Zoom' };
+
+export const withAStandardDiv = () => <Zoom><div>Hello Button</div></Zoom>;
+
+export const withAnImage = () => {
+  const Image = () => <img src="https://m.media-amazon.com/images/I/91FxiyDiVtL._SS500_.jpg" />;
+  return (
+    <Zoom>
+      <Image />
+    </Zoom>
+  );
+};
